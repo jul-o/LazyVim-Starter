7a44ddf3a302516bb55f1e1fa7cbfe0b9136b7c6
Jules Sang <jules.sang@grenoble-inp.org>
2025-02-09 18:07:24 +0100

    Deprioritize snippets

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,4 +1,24 @@
-return {}
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+  },
+  opts = function(_, opts)
+    local types = require("cmp.types")
+
+    -- Function to sort LSP snippets, so that they appear at the end of LSP suggestions
+    local function deprioritize_snippet(entry1, entry2)
+      if entry1:get_kind() == types.lsp.CompletionItemKind.Snippet then
+        return false
+      end
+      if entry2:get_kind() == types.lsp.CompletionItemKind.Snippet then
+        return true
+      end
+    end
+
+    table.insert(opts.sorting.comparators, 1, deprioritize_snippet)
+  end,
+}
 
 -- return {
 --   "hrsh7th/nvim-cmp",
