d11b4fccd559662b178e85eaaa9e05b9ce773173
Jules Sang <jules.sang@grenoble-inp.org>
2025-02-25 11:37:10 +0100

    Create :Redir redirecting command output to current buffer

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,3 +8,10 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
     vim.cmd("InlineFoldToggle")
   end,
 })
+
+vim.api.nvim_create_user_command("Redir", function(ctx)
+  local lines = vim.split(vim.api.nvim_exec(ctx.args, true), "\n", { plain = true })
+  vim.cmd("new")
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  vim.opt_local.modified = false
+end, { nargs = "+", complete = "command" })
